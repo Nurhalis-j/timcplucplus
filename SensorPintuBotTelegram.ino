8b2599c85789e062cfe41bc26b88207273595548
Nurhalis-j <52042008+Nurhalis-j@users.noreply.github.com>
2019-06-28 20:59:52 +0700

    Update SensorPintuBotTelegram.ino

--- SensorPintuBotTelegram.ino
+++ SensorPintuBotTelegram.ino
@@ -1,79 +1,85 @@
 // https://kelasrobot.com/cara-mudah-mengirim-pesan-dari-nodemcu-ke-telegram-telegrambot-part1/
 
-#include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>
+#include <ESP8266WiFi.h>          //include ke nodemcu
+#include <WiFiClientSecure.h>     //sembung ke client dengan secure
+#include <UniversalTelegramBot.h> //include bot telegram
 #define BUTTON D1
 
-
-char ssid[] = "Wifi-Prabayar"; 
-char password[] = "bukanwifigratismas";
-#define BOTtoken "881663171:AAHnbbTVRqoP7XaF83_gtJvyXa53E0oApSE"
-String chatid = "-286303573";
+char ssid[] = "Wifi-Prabayar";                                   // nama wifi
+char password[] = "bukanwifigratismas";                          // password wifi
+#define BOTtoken "881663171:AAHnbbTVRqoP7XaF83_gtJvyXa53E0oApSE" // bottoken telegram
+String chatid = "-286303573";                                    // chatid bot telegram
 int switchState, buffer_ = 1;
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
-void setup() {
-  Serial.begin(115200);
-  WifiStatus();
-  pinMode(BUTTON,INPUT_PULLUP);
+void setup()
+{
+    Serial.begin(115200);
+    WifiStatus();
+    pinMode(BUTTON, INPUT_PULLUP); //baca pin sensor pintu
 
-  delay(3000);
-  bot.sendMessage(chatid, "== Bot Terkoneksi ==\n> ");
-  Serial.println("\n\nSudah Siap Di Gunakan\n");
+    delay(3000);                                         //jarak waktu pesan
+    bot.sendMessage(chatid, "== Bot Terkoneksi ==\n> "); //pesan diserial
+    Serial.println("\n\nSudah Siap Di Gunakan\n");       //pesan diserial
 }
 
-void loop() {
-  switchState = digitalRead(BUTTON);
+void loop()
+{
+    switchState = digitalRead(BUTTON);
 
-  if (switchState == 1){
-    Serial.print("Pintu TERBUKA .... Nilai Sensor => ");
-    Serial.println(switchState);
+    if (switchState == 1)
+    {
+        Serial.print("Pintu TERBUKA .... Nilai Sensor => "); //pesan diserial
+        Serial.println(switchState);
 
-    if(buffer_ == 0){
-      Serial.print("\n\t\t VALUE BERUBAH ");
-      String data =  "[+] Pintu TERBUKA \n>  ";
-      bot.sendMessage(chatid,data);
-      Serial.println("\t\t Notif Telegram Terkirim\n ");
+        if (buffer_ == 0)
+        {
+            Serial.print("\n\t\t VALUE BERUBAH ");   //pesan diserial
+            String data = "[+] Pintu TERBUKA \n>  "; //pesan diserial
+            bot.sendMessage(chatid, data);
+            Serial.println("\t\t Notif Telegram Terkirim\n "); //pesan diserial
+        }
+
+        buffer_ = 1;
+        delay(1000); //jarak waktu pesan
     }
+    else if (switchState == 0)
+    {
+        Serial.print("Pintu tertutup .... Nilai Sensor => "); //pesan diserial
+        Serial.println(switchState);
 
-    buffer_ = 1;
-    delay(1000);
-    
-  }else if(switchState == 0){
-   Serial.print("Pintu tertutup .... Nilai Sensor => ");
-    Serial.println(switchState);
+        if (buffer_ == 1)
+        {
+            Serial.print("\n\t\t VALUE BERUBAH ");    //pesan diserial
+            String data = "[-] Pintu TERTUTUP \n>  "; //pesan diserial
+            bot.sendMessage(chatid, data);
+            Serial.println("\t\t Notif Telegram Terkirim\n "); //pesan diserial
+        }
 
-    if(buffer_ == 1){
-      Serial.print("\n\t\t VALUE BERUBAH ");
-      String data =  "[-] Pintu TERTUTUP \n>  ";
-      bot.sendMessage(chatid,data);
-      Serial.println("\t\t Notif Telegram Terkirim\n ");
+        buffer_ = 0;
+        delay(1000); //jarak waktu pesan
     }
-
-    buffer_ = 0;
-    delay(1000);
-    
-  }
 }
 
-void WifiStatus() {
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-  Serial.print("Connecting Wifi: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
+void WifiStatus()
+{
+    WiFi.mode(WIFI_STA); //mode station
+    WiFi.disconnect();
+    delay(100);                        //jarak waktu
+    Serial.print("Connecting Wifi: "); //pesan diserial
+    Serial.println(ssid);
+    WiFi.begin(ssid, password); //mulai connect ke wifi
 
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
+    while (WiFi.status() != WL_CONNECTED) //jika belum connect akan mengirim notif terus sampai connect
+    {
+        Serial.print(".");
+        delay(500); //jarak waktu
+    }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    Serial.println("");
+    Serial.println("WiFi connected"); //pesan diserial
+    Serial.print("IP address: ");     //pesan diserial
+    Serial.println(WiFi.localIP());   //pesan ip dari wifi
 }
